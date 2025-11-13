3e71a3511b286aefcb36a4353578b6cc69145e66
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-13 20:54:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h2>🚀 Flask rodando no Azure App Service!</h2>"
-
-@app.route("/api")
-def api():
-    return jsonify({"mensagem": "Olá, mundo!", "ambiente": os.getenv("AZURE_ENV", "local")})
+    return "Olá! Seu Web App Flask está rodando no Azure 👋"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
