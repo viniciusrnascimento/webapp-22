489a53608fd413f7bbd4610d886e2cf07b404c27
viniciusrnascimento <vinicius.rnascimento1@gmail.com>
2025-11-13 22:01:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Olá! Seu Web App Flask está rodando no Azure pelo GitHub Actions 🔥"
+    return "Olá! Swap feito no Azure pelo GitHub Actions 😎👍🏽"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
